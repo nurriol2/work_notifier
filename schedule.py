1ea65403cd117a9a1769d684d35fba7d75fe1045
nurriol2 <nurriol2@illinois.edu>
2020-07-25 21:50:41 -0500

    Refactor schedule data parsing
    Downloading a CSV of the online schedule and acessing the
    data via Google API handle "missing" data differently.
    
    Many of the "missing" cells are actually empty strings: ''.
    However, there are sparse NaN values. This module now accounts
    for both cases.
    
    NB:  The current version is not robust against repeated names
         in the instructor list.

--- schedule.py
+++ schedule.py
@@ -1,20 +1,37 @@
 #!/usr/bin/env python3
 
+from google_sheet import GoogleSheet
 import pandas as pd
 
 class Schedule:
 
-    def __init__(self, source=r"~/Desktop/projects/work_notifier/Foo Schedule Online - 7.20.csv"):
-        self.source = source 
-        self.content = self.process_schedule_from_csv()
+    def __init__(self, date):
+        self.date = date
+        self.content = self.process_schedule()
         return 
 
-    def process_schedule_from_csv(self):
-        df = pd.read_csv(self.source)
+    def process_schedule(self):
+        """Prepare a DataFrame to represent the online schedule
+
+        Returns:
+            pandas.DataFrame: Fully processed DataFrame representing the online schedule
+        """
+        df = GoogleSheet(self.date).sheet_to_dataframe()
         df = df.fillna(0)
-        df = df.rename(columns={'Unnamed: 0':"Instructors"})
+        #set the columns as timeslots 
+        df.columns = df.iloc[0]
+        #one of the empty string cells needs to be renamed
+        df = df.rename(columns={'':"Instructors"})
+        #the remaining empty string cells being replaced
+        df[df==''] = 0      
         return df
 
     def get_scheduled_instructors(self):
+        """The instructors scheduled to work on today's online schedule
+
+        Returns:
+            list: Names of the instructors in the instructor column of the online schedule. 
+                  Not robust to repeated names.
+        """
         df = self.content
         return list(df[df["Instructors"]!=0]["Instructors"])
