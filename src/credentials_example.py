965e0a0c50c5347a43ad93126580d70ec5775674
nurriol2 <nurriol2@illinois.edu>
2020-07-27 22:47:18 -0500

    Add an example of credentials

---
+++ src/credentials_example.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+credentials = {"account_sid":"The account SID from Twilio Console",
+                "auth_token":"The Twilio authentication token from Twilio Console",
+                "my_cell":"+verified phone numbers to send messages to",
+                "my_twilio":"+number purchased from Twilio",
+                "Google Secret File":"Downloaded from Google cloud authentication console",
+                "gsheetId":"44 characters before the gid in the Google Sheet URL"}
